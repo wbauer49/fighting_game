69dd246e6f5d558248f843a3526587f181ea368e
William Bauer <wbauer4949@gmail.com>
2022-11-26 21:38:13 -0800

    improved rendering runtime

--- rendering.py
+++ rendering.py
@@ -20,14 +20,14 @@ class Renderer:
             self.render_object(obj)
 
     def render_object(self, obj, x_offset=0, y_offset=0):
-        surface = pygame.Surface((WIDTH, HEIGHT))
+        surface = pygame.Surface((2 * obj.r, 2 * obj.r))
         surface.set_colorkey((0, 0, 0))
         surface.set_alpha(obj.get_color().a)
 
         center_x = WIDTH / 2 + obj.get_x() + x_offset
         center_y = HEIGHT / 2 - obj.get_y() - y_offset
-        pygame.draw.circle(surface, obj.get_color().as_tuple(), (center_x, center_y), obj.r)
-        self.screen.blit(surface, (0, 0))
+        pygame.draw.circle(surface, obj.get_color().as_tuple(), (obj.r, obj.r), obj.r)
+        self.screen.blit(surface, (center_x - obj.r, center_y - obj.r))
 
         for sub_object in obj.sub_objects:
             self.render_object(sub_object, x_offset=(obj.get_x() + x_offset), y_offset=(obj.get_y() + y_offset))
